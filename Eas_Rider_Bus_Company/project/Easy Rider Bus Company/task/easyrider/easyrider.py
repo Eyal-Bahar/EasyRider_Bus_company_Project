f9986fc0cd71cbedf55daa8818ab4a85db96ac59
Eyal_Bahar <eyal.bhr@gmail.com>
2022-04-09 19:54:17 +0700

    stage three completed!

--- Eas_Rider_Bus_Company/project/Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Eas_Rider_Bus_Company/project/Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 import json
-from collections import Counter
+from collections import Counter, defaultdict
 import inspect
 import time
 import re
@@ -82,14 +82,13 @@ class EzRider:
             match = checker(inp_value)
         else:
             match = checker == type(inp_value)
-        if not match:
-            self.data_type_errors[inp_field] += 1
         return match
 
     def check_if_filled(self, inp_field, inp_value) -> bool:
         empty = ""
         if inp_value == empty:
-            self.error_counter[inp_field] += 1
+            return False
+        return True
 
     def total_report_errors_found(self) -> dict:
 
@@ -102,45 +101,86 @@ class EzRider:
 
                 match = self.check_data_types(inp_field, inp_value)
                 # check is its filled (if required and matched)
+                if not match:
+                    self.data_type_errors[inp_field] += 1
+
                 required = inp_field in self.is_required
+                #  check if its filled - if it is required
                 if required and match:
-                    self.check_if_filled(inp_field, inp_value)
+                    is_filled = self.check_if_filled(inp_field, inp_value)
+                    if not is_filled:
+                        self.error_counter[inp_field] += 1
 
         self.total_errors_dict = Counter(self.data_type_errors) + Counter(self.error_counter)
 
     def get_tot_error(self) -> list:
         return sum(self.total_errors_dict.values())
 
+    def stops_counter(self, stand_alone=False):
+        if stand_alone:
+            self.stops_report = defaultdict(lambda: 0)
+            for data_point in self.parsed_input:
+                bus_id_instance = data_point["bus_id"]
+
+                # check if its filled according to format
+                match = self.check_data_types("bus_id", bus_id_instance)
+                required = "bus_id" in self.is_required
+                if not (required and match):
+                    continue
+                else:
+                    is_filled = self.check_if_filled("bus_id", bus_id_instance)
+
+                if is_filled:
+                    self.stops_report[bus_id_instance] += 1
+            return dict(self.stops_report)
+        else:
+            pass #Todo
 
 
-def stage_one(user_input):
-    ezrider = EzRider(user_input)
-    ezrider.total_report_errors_found()
-    tot_err = ezrider.get_tot_error()
-    report = [f"Type and required field validation: {tot_err} errors"]
-    for field in ezrider.fields:
-        report += [field + ": " + f"{ezrider.total_errors_dict[field]}"]
-    parsed_report = "\n".join(report)
-    return parsed_report
 
+def report_format(format_type, ezrider, stops_report=0, tot_err=0):
+    if format_type == "stage_three":
+        stops_report
+        report = ["Line names and number of stops:"]
+        for bus_id in stops_report.keys():
+            report += ["bus_id: " +  f"{bus_id}, " + f"stops: {stops_report[bus_id]}"]
+        parsed_report = "\n".join(report)
+        return parsed_report
 
-def report_format(format_type, tot_err, ezrider):
     if format_type == "stage_two":
         report = [f"Format validation: {tot_err} errors"]
         for field in ["stop_name", "stop_type", "a_time"]:
             report += [field + ": " + f"{ezrider.total_errors_dict[field]}"]
         parsed_report = "\n".join(report)
-    return parsed_report
+        return parsed_report
 
+    if format_type == "stage_one":
+        report = [f"Type and required field validation: {tot_err} errors"]
+        for field in ezrider.fields:
+            report += [field + ": " + f"{ezrider.total_errors_dict[field]}"]
+        parsed_report = "\n".join(report)
+        return parsed_report
+
+def stage_one(user_input):
+    ezrider = EzRider(user_input)
+    ezrider.total_report_errors_found()
+    tot_err = ezrider.get_tot_error()
+    parsed_report = report_format("stage_one", ezrider, tot_err=tot_err)
+    return parsed_report
 
 def stage_two(user_input):
     ezrider = EzRider(user_input)
     ezrider.total_report_errors_found()
     tot_err = ezrider.get_tot_error()
-    parsed_report = report_format("stage_two", tot_err, ezrider)
+    parsed_report = report_format("stage_two", ezrider, tot_err=tot_err)
     return parsed_report
 
+def stage_three(user_input):
+    ezrider = EzRider(user_input)
+    stops_report = ezrider.stops_counter(stand_alone=True)
+    parsed_report = report_format("stage_three", ezrider, stops_report=stops_report)
+    return parsed_report
 
 if __name__ == '__main__':
-    report = stage_two(input())
+    report = stage_three(input())
     print(report)
