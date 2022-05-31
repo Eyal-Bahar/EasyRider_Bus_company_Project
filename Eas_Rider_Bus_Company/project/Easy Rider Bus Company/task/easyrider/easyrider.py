ee02bbd221cf5c6ec9e16347cdd433bdeef60985
Eyal Bahar <80768687+Eyal-Bahar@users.noreply.github.com>
2022-05-31 23:54:04 +0700

    Added a project description

--- Eas_Rider_Bus_Company/project/Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Eas_Rider_Bus_Company/project/Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,3 +1,15 @@
+""" A project showcasing use OOP for very messy data validation.
+* Testing data consistency and correctness, 
+* JSON format data
+* Regular expression library.
+
+Simulating a bus company task for an uncpmlete database managment task : 
+
+You've just been hired by a bus company that started actively using the Internet for business. 
+Before you came, their database had been updated a few times by various employees with various levels of skill. 
+Your task is to find all the mistakes they made in the database. Good news: you have documentation, but bad news: it's incomplete. 
+This project helps to plunge into the real reality. A good workout before work. Respect to the developers!
+"""
 from dataclasses import dataclass, field
 import json
 from collections import Counter, defaultdict
